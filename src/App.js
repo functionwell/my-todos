66e8dae3c927b5476b27b50763cf7a45f44b1a68
fangke <fk@canonchain.com>
2018-09-21 18:30:19 +0800

    eject

--- src/App.js
+++ src/App.js
@@ -6,13 +6,41 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-menu">
+          <div className="search-wrap">
+            <input type="search" placeholder="搜索全部胶囊"/>
+          </div>
+          <div className="menu-list-wrap">
+            <h2 className="list-header">胶囊列表</h2>
+            <div className="todo-list">
+              <i className="iconfont icon-folder">icon</i>
+              <h3>全部胶囊</h3>
+              <div className="">6</div>
+            </div>
+            <div className="plan-list"></div>
+            <div className="done-list"></div>
+          </div>
+        </div>
+        <div className="App-cont">
+          <header className="cont-header">
+            <button type="button">刷新</button>
+            <h2>全部胶囊</h2>
+            <button type="button">扩展</button>
+          </header>
+          <main>
+            <div className="list-wrap">
+              <ul>
+                <li className="cont-todo">
+                  <input type="checkbox"/>
+                  <div>这是我的第一条代办事项</div>
+                </li>
+              </ul>
+            </div>
+          </main>
+          <footer className="cont-footer">
+            <button>新建胶囊</button>
+          </footer>
+        </div>
       </div>
     );
   }
